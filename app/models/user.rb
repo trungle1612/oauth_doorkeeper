bdc578e4327ae9ba727a6c1b5995f1730f8e4740
le xuan trung <lexuantrung1612@gmail.com>
2019-04-01 10:53:19 +0700

    Updating unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many(
+    :access_tokens,
+    class_name: 'Doorkeeper::AccessToken',
+    foreign_key: :resource_owner_id,
+    dependent: :destroy
+  )
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'mocha/minitest'
+
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+	class APIControllerTestCase < ActionController::TestCase
+	 def setup_api_controller(options = {})
+	    if options[:doorkeeper_authorized]
+	      @current_user = FactoryGirl.create(:user)
+	      doorkeeper_token = @current_user.doorkeeper_tokens.first || @current_user.doorkeeper_tokens.create
+	      doorkeeper_token.stubs(:acceptable?).returns(true)
+	      @controller.stubs(doorkeeper_token: doorkeeper_token)
+	      @controller.current_user.stubs(confirmed?: true) if options[:user_confirmed]
+	      @controller.current_user.stubs(verified?: true) if options[:user_confirmed]
+	    end
+	  @controller.stubs(:render)
+	 end
+  end
 end
